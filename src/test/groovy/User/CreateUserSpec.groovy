7d1b3879ea38d9dd6be0e329ad9a4dd954d138e3
ezhigda <daragh.higgins@ericsson.com>
2018-03-27 13:11:02 +0100

    Added initial vue project for ui

--- src/test/groovy/User/CreateUserSpec.groovy
+++ src/test/groovy/User/CreateUserSpec.groovy
@@ -1,12 +1,28 @@
 package User
 
+import com.daraghhiggins.shareprofile.User.User
+import com.daraghhiggins.shareprofile.Utils.HibernateUtil
+import org.hibernate.Session
 import spock.lang.Specification
+import spock.lang.Stepwise
 
+@Stepwise
 class CreateUserSpec extends Specification {
 
-    def 'base test' () {
+    def 'setup' () {
+
+    }
+
+    def 'Create and delete user' () {
         given:
-            def i = 3
+            Session session = HibernateUtil.getSession().openSession()
+            session.beginTransaction()
+        and:
+            User user = new User();
+            user.setUser_name("admin5515");
+            user.setFirstName("Daragh");
+            user.setSurnName("Higgins");
+            user.setPassword("password");
         when:
             i = i + 1
         then:
