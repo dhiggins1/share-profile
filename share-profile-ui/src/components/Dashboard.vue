88a61c548699f76aac54fc53457286ac3edea3ec
ezhigda <daragh.higgins@ericsson.com>
2018-04-04 14:12:07 +0100

    Fetch coin list and get user value of investment

--- share-profile-ui/src/components/Dashboard.vue
+++ share-profile-ui/src/components/Dashboard.vue
@@ -3,11 +3,17 @@
     <md-button class="md-dense md-raised md-primary" v-on:click="cryptoRequest()">Populate Coin Base</md-button>
     <div class="md-layout md-gutter">
       <div class="md-layout-item md-size-100">
+        <div class="inputInvestment">
         <md-field id="usersField">
-          <v-select multiple :options="currencyCode" v-model="codes" placeholder="Users" style="width:100%;"></v-select>
-          <span class="md-error">The dashboard users must be selected</span>
+          <v-select :options="currencyCode" :on-change="getCurrencySelected" placeholder="Currency" style="width:100%;"></v-select>
         </md-field>
-      </div>
+        <md-field>
+          <label>Enter Investment Total</label>
+          <md-input v-model="amountInvested"></md-input>
+        </md-field>
+        </div>
+          <md-button class="md-dense md-raised md-primary" v-on:click="getInvestmentValue(amountInvested, sourceCurrency)">Enter Value</md-button>
+        </div>
     </div>
   </div>
 </template>
@@ -21,7 +27,9 @@ export default {
       sourceCurrency: null,
       destinationCurrency: null,
       currencyCode: [],
-      currencyCodeToName: {}
+      currencyCodeToName: [],
+      amountInvested: 0,
+      calculatedValue: 0
     }
   },
   methods: {
@@ -32,10 +40,31 @@ export default {
           var i = 0
           Object.keys(response.data.Data).forEach(function (key) {
             self.currencyCode.push({label: response.data.Data[key].FullName, value: i})
+            self.currencyCodeToName[response.data.Data[key].FullName] = key
             i++
           })
-          console.log(self.currencyCode)
+          console.log(self.currencyCodeToName)
         })
+    },
+
+    getInvestmentValue (currencyValue, currencyCode) {
+      let self = this
+      let requestURL = 'https://min-api.cryptocompare.com/data/price?fsym=' + this.sourceCurrency + '&tsyms=EUR'
+      this.axios.get(requestURL)
+        .then(response => {
+          let currRate = response.data.EUR
+          this.calculateCurrencyConversion(currRate)
+          console.log(this.calculatedValue)
+        })
+      console.log(this.sourceCurrency)
+    },
+
+    getCurrencySelected (value) {
+      this.sourceCurrency = this.currencyCodeToName[value.label]
+    },
+
+    calculateCurrencyConversion (currRate) {
+      this.calculatedValue = currRate * this.amountInvested
     }
   },
   components: {
@@ -45,5 +74,14 @@ export default {
 </script>
 
 <style scoped>
+#usersField {
+  width: 25%;
+  height: 25%;
+  margin: 0 auto;
+}
 
+.inputInvestment {
+
+  margin: 0 auto;
+}
 </style>
